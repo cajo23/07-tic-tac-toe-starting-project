cb68908a7bf7cda407db1b755290f1673d2dfe4c
cajo23 <143487612+cajo23@users.noreply.github.com>
2024-04-06 00:15:34 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import Log from "./components/Log.jsx";
 import GameOver from "./components/GameOver.jsx";
 import { WINNING_COMBINATIONS } from "./components/winning-combinations.js";
 
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+};
+
 //Spelplan
-const initialGameBoard = 
+const INITIAL_GAME_BOARD = 
 [
     [null, null, null],
     [null, null, null],
@@ -23,12 +28,8 @@ function deriveActivePlayer(gameTurns){
       return currentPlayer;
 }
 
-function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard =[...initialGameBoard.map(array => [...array])];
+function deriveGameBoard(gameTurns){
+  let gameBoard =[...INITIAL_GAME_BOARD.map((array) => [...array])];
   
   for(const turn of gameTurns){
       const {square, player} = turn;
@@ -37,6 +38,10 @@ function App() {
       gameBoard[row] [col] = player;
   }
 
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players){
   let winner;
 
   for(const combination of WINNING_COMBINATIONS){
@@ -48,14 +53,25 @@ function App() {
     gameBoard[combination[2].row][combination[2].column];
 
     if(
-      firstSquareSymbol && firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol &&
        firstSquareSymbol === thirdSquareSymbol
       )
        {
-        winner = firstSquareSymbol;
+        winner = players[firstSquareSymbol];
     }
   }
 
+  return winner;
+}
+
+function App() {
+  const [ players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   //Funktion som växlar mellan spelare X och O, när en ruta väljs på GameBoard
@@ -81,14 +97,32 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return{
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return (
   <main>
     <div id="game-container">
      <ol id="players" className="highlight-player">
       {/* Skapa två instanser av Player-komponenten med olika namn och symboler,
       Dessa är isolerade från varandra*/}
-     <Player initialName = "Player 1" symbol= "X" isActive={activePlayer === 'X'}/>
-     <Player initialName = "Player 2" symbol= "O" isActive={activePlayer === 'O'}/>
+     <Player 
+     initialName = {PLAYERS.X}
+     symbol= "X" 
+     isActive={activePlayer === 'X'}
+     onChangeName={handlePlayerNameChange}/>
+
+     <Player 
+     initialName = {PLAYERS.O}
+     symbol= "O" 
+     isActive={activePlayer === 'O'}
+     onChangeName={handlePlayerNameChange}/>
      </ol>
      {(winner || hasDraw) && (
      <GameOver winner={winner} onRestart={handleRestart}/>
